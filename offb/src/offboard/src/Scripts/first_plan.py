2d82d2fbc63f4eef5a5aa7f61c583c84bbcdeea4
TheRoadToReality <86977203+TheRoadToReality@users.noreply.github.com>
2023-07-03 16:41:37 +0800

    Update first_plan.py

--- offb/src/offboard/src/Scripts/first_plan.py
+++ offb/src/offboard/src/Scripts/first_plan.py
@@ -15,7 +15,7 @@ current_state = State()
 def state_cb(msg):
     global current_state
     current_state = msg
-"回调函数定义在订阅之前"
+
 #def waypoint(msg):
 #   x_location = msg.pose.position.x
 #   y_location = msg.pose.position.y
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     rospy.init_node("plan1")
 
 
-    now_pos_pub = rospy.Subscriber("mavros/local_position/pose",PoseStamped, callback=waypoint)
+#    now_pos_pub = rospy.Subscriber("mavros/local_position/pose",PoseStamped, callback=waypoint)
     local_pos_pub = rospy.Publisher("mavros/setpoint_position/local", PoseStamped, queue_size=10)
     state_sub = rospy.Subscriber("mavros/state", State, callback = state_cb)
     rospy.wait_for_service("/mavros/cmd/arming")
@@ -52,13 +52,13 @@ if __name__ == "__main__":
 
     a = input()
     if (a == 1):
-        x_1 = 0.43
-        y_1 = 2.04
+        x_1 = 0.44
+        y_1 = 1.80
 
     b = input()
-    if(b ==1 ):
-        x_2 = 1.52
-        y_2 = 1.08
+    if(b ==2 ):
+        x_2 = 1.42
+        y_2 = 0.90
     for i in range(100):
         if(rospy.is_shutdown()):
             break
@@ -84,62 +84,3 @@ if __name__ == "__main__":
                 last_req = rospy.Time.now()
             elif not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0):
                 if arming_client.call(arm_cmd).success == True:
-                    rospy.loginfo("Vehicle armed")
-                last_req = rospy.Time.now()
-                flag = flag + 1
-            elif flag==1 and (rospy.Time.now() - last_req) > rospy.Duration(10.0):
-                pose.pose.position.x = 0
-                pose.pose.position.y = 0
-                pose.pose.position.z = 0.6
-                last_req = rospy.Time.now()
-                flag = flag + 1
-            elif flag==2 and x_1 is not None and y_1 is not None and z_1 is not None and (rospy.Time.now() - last_req) > rospy.Duration(10.0):
-                pose.pose.position.x = x_1
-                pose.pose.position.y = y_1
-                pose.pose.position.z = 0.6
-                last_req = rospy.Time.now()
-                flag = flag + 1
-            elif flag==3 and x_1 is not None and y_1 is not None and z_1 is not None and (rospy.Time.now() - last_req) > rospy.Duration(10.0):
-                pose.pose.position.x = x_1
-                pose.pose.position.y = y_1
-                pose.pose.position.z = 0.3
-                last_req = rospy.Time.now()
-                flag = flag + 1
-            elif flag==4 and x_1 is not None and y_1 is not None and z_1 is not None and (rospy.Time.now() - last_req) > rospy.Duration(10.0):
-                pose.pose.position.x = x_1
-                pose.pose.position.y = y_1
-                pose.pose.position.z = 0.6
-                last_req = rospy.Time.now()
-                flag = flag + 1
-            elif flag==5 and x_2 is not None and y_2 is not None and z_2 is not None and (rospy.Time.now() - last_req) > rospy.Duration(10.0):
-                pose.pose.position.x = x_2
-                pose.pose.position.y = y_2
-                pose.pose.position.z = 0.6
-                last_req = rospy.Time.now()
-                flag = flag + 1
-            elif flag==6 and x_2 is not None and y_2 is not None and z_2 is not None and (rospy.Time.now() - last_req) > rospy.Duration(10.0):
-                pose.pose.position.x = x_2
-                pose.pose.position.y = y_2
-                pose.pose.position.z = 0.3
-                last_req = rospy.Time.now()
-                flag = flag + 1
-            elif flag==7 and x_2 is not None and y_2 is not None and z_2 is not None and (rospy.Time.now() - last_req) > rospy.Duration(10.0):
-                pose.pose.position.x = x_2
-                pose.pose.position.y = y_2
-                pose.pose.position.z = 0.6
-                last_req = rospy.Time.now()
-                flag = flag + 1
-            elif flag== 8 and x_2 is not None and y_2 is not None and z_2 is not None and (rospy.Time.now() - last_req) > rospy.Duration(10.0):
-                pose.pose.position.x = 0
-                pose.pose.position.y = 0
-                pose.pose.position.z = 0.6
-                last_req = rospy.Time.now()
-                flag = flag + 1                         
-            elif flag== 9 and current_state.mode != "AUTO.LAND" and (rospy.Time.now() - last_req) > rospy.Duration(10.0):
-                if set_mode_client.call(land_set_mode).mode_sent == True:
-                    rospy.loginfo("LAND enabled")
-                last_req = rospy.Time.now()
-            elif flag == 10 and (rospy.Time.now() - last_req) > rospy.Duration(5.0):
-                break
-            local_pos_pub.publish(pose)
-            rate.sleep()
